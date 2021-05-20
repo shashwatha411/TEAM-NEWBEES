b4550818330d11b0440543212d6f24b2e046cf7e
shashwatha411 <shashwatha411@gmail.com>
2021-05-20 20:36:18 +0530

    Organise Files

---
+++ Chatbot/responses.js
@@ -0,0 +1,12 @@
+function getBotResponse(input) {
+    if (input == "1")
+        return "Click on nav bar";
+    else if (input == "2")
+        return "Click on nav bar";
+    else if (input == "3")
+        return "Click on nav bar";
+    else if (input == "4")
+        return "Click on nav bar";
+    else
+        return "Try again. Type the exact command again. ";
+}
